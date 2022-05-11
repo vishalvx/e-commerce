e96fd3e1a1f2bfa432e6e055b4f83522f1f827ff
Vishal Prajapati <64828902+vishalvx@users.noreply.github.com>
2022-05-11 14:10:54 +0530

    JWT Errors,Duplicate Keys Error Handled

--- backend/src/middleware/error.js
+++ backend/src/middleware/error.js
@@ -10,6 +10,24 @@ export default (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  // Duplicate Key Error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is Invalid, Try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //JWT EXPIRE Error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is Expired, Try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
